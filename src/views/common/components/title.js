507f9adbb735511976d7db32634e8f10631f1cd0
jjol <jjoulk@gmail.com>
2019-11-18 22:39:02 -0600

    Deleted Files

--- src/views/common/components/title.js
+++
@@ -1,8 +0,0 @@
-class TitleComponent {
-    constructor(elId, language, sessionName) {
-        this.container = document.querySelector(elId);
-        this.container.querySelector('h2').innerHTML = sessionName;
-    }
-}
-
-module.exports = TitleComponent;
